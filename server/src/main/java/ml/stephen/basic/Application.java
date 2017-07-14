52f907035e0a57e6e520ff9c50899b70be8358f6
stephen <stephen.ml@hotmail.com>
2017-07-14 11:15:42 +0800

    设置打包后的默认首页 node_modules版本号统一

--- server/src/main/java/ml/stephen/basic/Application.java
+++ server/src/main/java/ml/stephen/basic/Application.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by Stephen on 2017/7/10.
@@ -23,6 +27,28 @@ public class Application {
         ServletRegistrationBean bean = new ServletRegistrationBean(servlet, "/api/*");
         bean.setName("ApiServlet");
         return bean;
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry viewControllerRegistry) {
+
+                // 首页默认加载web端
+                viewControllerRegistry.addViewController("/").setViewName("/web/index.html");
+                viewControllerRegistry.addViewController("/index.html").setViewName("redirect:/");
+
+                // web首页
+                viewControllerRegistry.addViewController("/web").setViewName("/web/index.html");
+
+                // webapp首页
+                viewControllerRegistry.addViewController("/webapp").setViewName("/webapp/index.html");
+
+                viewControllerRegistry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+                super.addViewControllers(viewControllerRegistry);
+            }
+        };
     }
 
 }
--- web/config/index.js
+++ web/config/index.js
@@ -1,7 +1,7 @@
 // see http://vuejs-templates.github.io/webpack for documentation.
 var path = require('path')
 
-var assetsRoot = path.resolve(__dirname, '../build/resources/main/static')
+var assetsRoot = path.resolve(__dirname, '../build/resources/main/static/web')
 
 module.exports = {
 	build: {
@@ -9,7 +9,7 @@ module.exports = {
 		index: path.resolve(assetsRoot, 'index.html'),
 		assetsRoot: assetsRoot,
 		assetsSubDirectory: 'assets',
-		assetsPublicPath: '/',
+		assetsPublicPath: '/web/',
 		productionSourceMap: true,
 		// Gzip off by default as many popular static hosts such as
 		// Surge or Netlify already gzip all static assets for you.
